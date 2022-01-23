0b917b22be172916248e179765256ba16293d0df
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2022-01-23 14:44:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,45 +1,72 @@
-from os import environ
+import os
 import aiohttp
 from pyrogram import Client, filters
 
-API_ID = environ.get('API_ID')
-API_HASH = environ.get('API_HASH')
-BOT_TOKEN = environ.get('BOT_TOKEN')
-API_KEY = environ.get('API_KEY')
-
-bot = Client('shortner bot',
-             api_id=API_ID,
-             api_hash=API_HASH,
-             bot_token=BOT_TOKEN,
-             workers=50,
-             sleep_threshold=10)
+API_ID = os.environ.get('API_ID')
+API_HASH = os.environ.get('API_HASH')
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+API_KEY = os.environ.get('API_KEY')
+WEBSITE = os.environ.get('WEBSITE')
 
 
-@bot.on_message(filters.command('start') & filters.private)
+try:
+    bot = Client('shortener bot',
+                 api_id=API_ID,
+                 api_hash=API_HASH,
+                 bot_token=BOT_TOKEN,
+                 workers=50,
+                 sleep_threshold=10)
+except Exception:
+    print("Add var values properly. Read readme.md once")
+
+
+@bot.on_message(filters.command('start'))
 async def start(bot, message):
-    await message.reply(
-        f"**Hi {message.chat.first_name}!**\n\n"
-        "I'm GPlink bot. Just send me link and get short link")
+    start_msg = f"""
+Hi {message.chat.first_name}!
+
+I'm {WEBSITE} bot. Just send me link and get short link!
+
+Send me a link to short a link with random alias.
+
+For custom alias, <code>[link] | [custom_alias]</code>, Send in this format\n
+Ex: https://t.me/example | Example
+
+    """
+    await message.reply_text(start_msg, disable_web_page_preview=True, quote=True)
 
 
 @bot.on_message(filters.regex(r'https?://[^\s]+') & filters.private)
 async def link_handler(bot, message):
-    link = message.matches[0].group(0)
-    try:
-        short_link = await get_shortlink(link)
-        await message.reply(f'Here is your [short link]({short_link})', quote=True)
-    except Exception as e:
-        await message.reply(f'Error: {e}', quote=True)
+    if "|" in message.text:
+        link_parts = message.text.split("|")
+        link = link_parts[0]
+        aliases = link_parts[1:len(message.text) + 1]
+        alias1 = ""
+        for alias in aliases:
+            alias1 += alias
+        x = alias1.replace(" ", "")
+    else:
+        link = message.matches[0].group(0)
+        x= ""
+    short_link = await get_shortlink(link, x)
+    await message.reply(short_link, quote=True)
 
 
-async def get_shortlink(link):
-    url = 'https://gplinks.in/api'
-    params = {'api': API_KEY, 'url': link}
+async def get_shortlink(link, x):
+    url = f'https://{WEBSITE}/api'
+    params = {'api': API_KEY,
+              'url': link,
+              'alias': x
+              }
 
     async with aiohttp.ClientSession() as session:
-        async with session.get(url, params=params, raise_for_status=True) as response:
+        async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
             data = await response.json()
-            return data["shortenedUrl"]
-
+            print(data["status"])
+            if data["status"] == "success":
+                return f"<code>{data['shortenedUrl']}</code>\n\nHere is your Link:\n{data['shortenedUrl']}"
+            else:
+                return f"Error: {data['message']}"
 
 bot.run()
